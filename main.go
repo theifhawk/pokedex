c42970369db7f9885d50c41670d982bfd4b65c40
Theifhawk <wchen291@gmail.com>
2025-05-07 18:41:49 -0400

    get requests to pokeapi for map locations

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"pokedex/internal/pokeapi"
+	"time"
+)
+
 func main() {
-	startRepl()
+	pokeClient := pokeapi.NewClient(5 * time.Second)
+	cfg := &config{
+		pokeapiClient: pokeClient,
+	}
+
+	startRepl(cfg)
 }
